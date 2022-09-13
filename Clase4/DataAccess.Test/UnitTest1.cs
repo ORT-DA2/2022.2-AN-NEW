a1a9aba94932112e24f47da317da3b8663639d48
Marco Fiorito <marcofiorito1@gmail.com>
2022-09-13 15:03:45 -0300

    add basic DataAccess project

---
+++ Clase4/DataAccess.Test/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataAccess.Test;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
