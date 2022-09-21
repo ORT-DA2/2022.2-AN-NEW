7812d75602a1d00173847e0ec9db55391549968d
Marco Fiorito <marcofiorito1@gmail.com>
2022-09-21 18:28:19 -0300

    duplicate project

---
+++ Clase5/Domain/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Domain;
+
+public class Order: IEqualityComparer<Order>
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Address  { get; set; }
+    public int PutchaseNumber { get; set; }
+    public int Price { get; set; }
+    public DateTime DeliveryDateTima { get; set; }
+
+    public bool Equals(Order x, Order y)
+    {
+	    if (ReferenceEquals(x, y)) return true;
+	    if (ReferenceEquals(x, null)) return false;
+	    if (ReferenceEquals(y, null)) return false;
+	    if (x.GetType() != y.GetType()) return false;
+	    return x.Id == y.Id && x.Name == y.Name;
+    }
+
+    public int GetHashCode(Order obj)
+    {
+	    return HashCode.Combine(obj.Id, obj.Name);
+    }
+}
---
+++ Clase5/WebAPI/Startup.cs
@@ -0,0 +1,56 @@
+using BusinessLogic;
+using DataAccess;
+using IBusinessLogic;
+using IDataAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI;
+
+public class Startup
+{
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowEverything",builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+            });
+            services.AddControllers();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<IOrderService, OrderService>();
+            string directory = System.IO.Directory.GetCurrentDirectory();
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(directory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var connectionString = configuration.GetConnectionString("DBClase2");
+            services.AddDbContext<ContextDb>(options =>
+                options.UseSqlServer(connectionString));
+         
+        }
+            
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
