7a25ba9b0d559b07a8ca47d3cc4f1671809291ef
Marco Fiorito <marcofiorito1@gmail.com>
2022-09-29 14:38:10 -0300

    add importers

---
+++ Clase6EjemploAvanzado/XmlImporter/XmlImporter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Domain;
+using IImporter;
+
+namespace XmlImporter;
+// Este proyecto podria estar en otra solucion tranquilamente, solo necesito el dll
+// que resulta de compilar el proyecto para ponerlo en la carpeta Importers
+public class XmlImporter : IImporter
+{
+    public string GetName()
+    {
+        return "Xml Importer";
+    }
+
+    // Aca obviamente va a leer de un JSON, no devolver algo hardcodeado
+    public List<Movie> ImportOrders()
+    {
+        List<Order> ordersToReturn = new List<Order>()
+        {
+            new Order()
+            {
+                Id = 1,
+                Name = "Papas fritas",
+                Address = "Juan Gomez 3878",
+                PutchaseNumber = 1,
+                Price = 100,
+                DeliveryDateTima = DateTime.Now.AddHours(1)
+            },
+            new Order()
+            {
+                Id = 2,
+                Name = "Milanesa",
+                Address = "Juan Gomez 3878",
+                PutchaseNumber = 1,
+                Price = 150,
+                DeliveryDateTima = DateTime.Now.AddHours(1)
+            }
+        };    
+    }
+}
