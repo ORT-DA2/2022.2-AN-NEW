d3ee572545ad43982a2fb88ba8ba81928e303aaa
Marco Fiorito <marcofiorito1@gmail.com>
2022-09-29 14:31:32 -0300

    add IImporter

---
+++ Clase6EjemploAvanzado/IImporter/IImporter.cs
@@ -0,0 +1,17 @@
+﻿using Domain;
+
+namespace IImporter;
+public interface IImporter
+{
+    string GetName();
+
+    // 2 Cosas a notar aqui:
+    // - Hacemos referencia a Order, por lo que deberiamos compartir el proyecto domain tambien a 
+    // diferencia de lo que dijimos arriba, aca podemos crear otros DTOs para esto y usar el patron Adapter
+    // - No recibimos parametros, estaria bueno representar los parametros de alguna forma bastante generica
+    // para pasar info necesaria a cada importador
+    List<Order> ImportOrders();
+    
+    // Ej de posible manejo de parametros
+    // List<Parameter> GetNeededParameters();
+}
